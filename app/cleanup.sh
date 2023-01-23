bf25a45c67fb4ddbf6fec7f2b4feb98f4489be80
Pox <ipox@live.com>
2023-01-23 03:30:41 +0000

    changing iptables to -nft
    test

--- app/cleanup.sh
+++ app/cleanup.sh
@@ -8,7 +8,7 @@ LOCAL_IP=$(ip addr show $DEVICE_NAME | grep "inet\b" | awk '{print $2}')
 if [ -z ${LOCAL+x} ] || [ "$LOCAL" = "true" ]; then
     for i in $(ip addr show $DEVICE_NAME | grep "inet\b" | awk '{print $2}'); do
         while IFS="" read -r SERVER_IP || [ -n "$SERVER_IP" ]; do
-            iptables -t nat -D POSTROUTING -s $i -d $SERVER_IP -o $DEVICE_NAME -j MASQUERADE
+            iptables-nft -t nat -D POSTROUTING -s $i -d $SERVER_IP -o $DEVICE_NAME -j MASQUERADE
         done < server-list.txt
     done
 else
@@ -26,10 +26,10 @@ else
             VPN_SUBNET=$(ip addr show ${!var} | grep "inet\b" | awk '{print $2}')
 
             while IFS="" read -r SERVER_IP || [ -n "$SERVER_IP" ]; do
-                iptables -t nat -D POSTROUTING -s $VPN_SUBNET -d $SERVER_IP -o $DEVICE_NAME -j MASQUERADE
+                iptables-nft -t nat -D POSTROUTING -s $VPN_SUBNET -d $SERVER_IP -o $DEVICE_NAME -j MASQUERADE
             done < server-list.txt
-            iptables -D FORWARD -i $DEVICE_NAME -o ${!var} -m state --state RELATED,ESTABLISHED -j ACCEPT
-            iptables -D FORWARD -i ${!var} -o $DEVICE_NAME -j ACCEPT
+            iptables-nft -D FORWARD -i $DEVICE_NAME -o ${!var} -m state --state RELATED,ESTABLISHED -j ACCEPT
+            iptables-nft -D FORWARD -i ${!var} -o $DEVICE_NAME -j ACCEPT
         done
     fi
 fi
@@ -37,5 +37,5 @@ fi
 # Clean up mitigate.py
 rm /app/'<stdin>.cleanup.sh'
 while IFS="" read -r SERVER_IP || [ -n "$SERVER_IP" ]; do
-    iptables -t nat -S PREROUTING | grep $SERVER_IP | cut -d " " -f 2- | xargs -rL1 iptables -t nat -D
+    iptables-nft -t nat -S PREROUTING | grep $SERVER_IP | cut -d " " -f 2- | xargs -rL1 iptables-nft -t nat -D
 done < server-list.txt
